6158cf4a53218bcc67822a53f29ca2f449017088
Rishika-05 <rishikaraj7263@gmail.com>
2023-03-21 10:50:18 +0530

    initial commit

---
+++ src/test/java/com/se/wastemanagementsystem/WasteManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.se.wastemanagementsystem;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+
+class WasteManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello");
+    }
+
+}
